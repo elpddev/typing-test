c683e741cd2f6b0846398f37e83cc4ae2224e562
Eyal Lapid <elpddev@pm.me>
2022-10-14 08:13:40 +0300

    add state manager hook. add ui main parts

--- src/TypingGame.tsx
+++ src/TypingGame.tsx
@@ -1,3 +1,69 @@
+import { Container } from '@mantine/core';
+import { Stack, NumberInput, Text, Group, Button } from '@mantine/core';
+import { useCallback, useState } from 'react';
+
 export function TypingGame() {
-  return (<div>Typing game</div>);
+  const {wpm, timeLeft, restart} = useTypingGame();
+
+  return (
+    <Container>
+      <Stack align="center">
+        <div>Game Title</div>
+
+        <ActionBar wpm={wpm} timeLeft={timeLeft} onRestart={restart}/>
+
+        <WordsCard />
+
+        <TypingPanel />
+      </Stack>
+    </Container>
+  );
+}
+
+function ActionBar({ wpm, timeLeft, onRestart }: {
+  wpm: number;
+  timeLeft: number;
+  onRestart: () => void;
+}) {
+  return (
+    <Group>
+      <Text>WPM</Text>
+      <NumberInput disabled value={wpm}/>
+
+      <Text>Time left</Text>
+      <NumberInput disabled value={timeLeft}/>
+
+      <Button onClick={onRestart}>Restart</Button>
+    </Group>
+  )
+}
+
+function WordsCard() {
+  return (<div>Word card</div>);
+}
+
+function TypingPanel() {
+  return (<div>Typing panel</div>);
+}
+
+enum GameStatus {
+  Initial,
+  InProgress,
+  Done,
+}
+
+function useTypingGame() {
+  const [wpm, setWpm] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(0);
+  const [gameStatus, setGameStatus] = useState<GameStatus>(GameStatus.Initial);
+
+  const restart = useCallback(() => {
+    console.log('** restart');
+  }, []);
+
+  return {
+    wpm,
+    timeLeft,
+    restart
+  };
 }
