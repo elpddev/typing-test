f108dcff2c53dcccf5e1b6908d9db384aef585b6
Eyal Lapid <elpddev@pm.me>
2022-10-21 13:15:20 +0300

    fix generic typing

--- src/utils/getRandomItem.ts
+++ src/utils/getRandomItem.ts
@@ -1,6 +1,6 @@
 import { wordBank } from "../data/wordBank";
 
-export function getRandomItem<T>(items: any[]): T {
+export function getRandomItem<T>(items: T[]): T {
   const nextIndex = Math.floor(Math.random() * (items.length - 1));
   const nextItem = items[nextIndex];
 
