ac20e78ff42205f142ec92266cff8ebc185258a1
Eyal Lapid <elpddev@pm.me>
2022-10-22 19:08:44 +0300

    refactor out function

--- src/components/TypingGame.tsx
+++ src/components/TypingGame.tsx
@@ -4,15 +4,10 @@ import { useCallback, useEffect, useState } from "react";
 import { ActionBar } from "./ActionBar";
 import { KeyCode } from "../KeyCode";
 import { WordsCard } from "./WordsCard";
-import {
-  Board,
-  init,
-  moveByKey,
-  getLetterSuccessAmount,
-} from "../models/Board";
-import { useInterval } from "../utils/useInterval";
+import { init, moveByKey } from "../models/Board";
 import { useCountdown } from "../utils/useCountdown";
 import { useTypingCapture } from "../utils/useTypingCapture";
+import { useMeasureWpm } from "./useMeasureWpm";
 
 export function TypingGame() {
   const { wpm, timeLeft, board, restart } = useTypingGame();
@@ -86,38 +81,5 @@ function useTypingGame() {
     timeLeft,
     restart,
     onTyping,
-  };
-}
-
-function useMeasureWpm(board: Board) {
-  const { start: startInterval, stop: stopInterval } = useInterval();
-  const [wpm, setWpm] = useState(0);
-  const [boardLocal, setBoardLocal] = useState(board);
-
-  const start = useCallback(() => {
-    startInterval(200, (timePassed) => {
-      setBoardLocal((currBoard) => {
-        const successAmount = getLetterSuccessAmount(currBoard);
-        const wpmTemp = ((successAmount / (timePassed / 1000)) * 60) / 5;
-
-        setWpm(wpmTemp);
-
-        return currBoard;
-      });
-    });
-  }, []);
-
-  const stop = useCallback(() => {
-    stopInterval();
-  }, []);
-
-  useEffect(() => {
-    setBoardLocal(board);
-  }, [board]);
-
-  return {
-    wpm,
-    start,
-    stop,
   };
 }
